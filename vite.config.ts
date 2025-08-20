898f584df7110ac0c4325d666b9a0ab99d1e7aaa
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2025-08-20 23:04:45 +0000

    Add GitHub Pages auto-deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/token-visualizer/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
